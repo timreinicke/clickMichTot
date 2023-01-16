6d12b0e6b6760799e4ef75f96d81a69afe52a827
timreinicke <45878428+timreinicke@users.noreply.github.com>
2023-01-16 19:15:43 +0100

    Added Circles

--- src/main/java/presentation/scenes/gameScreen/GameScreenController.java
+++ src/main/java/presentation/scenes/gameScreen/GameScreenController.java
@@ -4,6 +4,7 @@ import javafx.animation.TranslateTransition;
 import javafx.application.Platform;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
 import javafx.util.Duration;
 
 import java.io.FileNotFoundException;
@@ -26,13 +27,31 @@ public class GameScreenController extends Thread{
                 String id = i+"."+j;
                 Button tap = new Button(String.valueOf(j));
                 buttonManager.put(id, tap);
+
                 /*
                 int x = (int)(Math.random() * view.getWidth());
                 int y = (int)(Math.random() * view.getHeight());
                 tap.setLayoutX(x);
                 tap.setLayoutX(y);*/
 
-                Timer buttonDisable = new Timer();
+                  int x = (int)(Math.random() * view.getWidth());
+                  int y = (int)(Math.random() * view.getHeight());
+                  tap.setLayoutX(x);
+                  tap.setLayoutY(y);
+                  Circle circle  = new Circle(x, y, 50);
+                  circle.setLayoutX(x);
+                  circle.setLayoutY(y);
+                  Platform.runLater(() -> {
+                      view.getChildren().add(tap);
+                      view.getChildren().add(circle);
+                  });
+
+                  try {
+                      Thread.sleep(1000);
+                  } catch (InterruptedException e) {
+                      throw new RuntimeException(e);
+                  }
+                  Timer buttonDisable = new Timer();
                 buttonDisable.schedule(new TimerTask() {
                     @Override
                     public void run() {
@@ -40,9 +59,12 @@ public class GameScreenController extends Thread{
                             if(view.getChildren().contains(buttonManager.get(id))) {
                                 view.getChildren().remove(buttonManager.get(id));
                             }
+
+
+                            tap.getStyleClass().add("button-click");
                         });
                     }
-                }, 5000);
+                }, 1000);
 
                 tap.setId(i+ "."+ j);
                 tap.setOnAction(e -> {
@@ -51,7 +73,6 @@ public class GameScreenController extends Thread{
                     buttonManager.remove(id);
 
                 });
-                view.getChildren().add(tap);
               }
 
           }
