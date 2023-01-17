1e30c8e8c64b36b97a18983bd330e4939d7619b9
timreinicke <45878428+timreinicke@users.noreply.github.com>
2023-01-17 16:38:54 +0100

    Fixed Button Positioning and Style, Fixed Character Positioning

--- src/main/java/presentation/scenes/gameScreen/GameScreenController.java
+++ src/main/java/presentation/scenes/gameScreen/GameScreenController.java
@@ -2,6 +2,7 @@ package presentation.scenes.gameScreen;
 
 import javafx.animation.TranslateTransition;
 import javafx.application.Platform;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Circle;
@@ -13,75 +14,78 @@ import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class GameScreenController extends Thread{
+public class GameScreenController extends Thread {
 
     private GameScreenView view;
     private ImageView hero_view;
+    private Canvas gameCanvas;
 
     public Thread buttonSpawn() {
-      return new Thread(() -> {
-          Map<String, Button> buttonManager = new HashMap<>();
+        return new Thread(() -> {
+            Map<String, Button> buttonManager = new HashMap<>();
 
-          for(int i = 0; i<3; i++){
-              for(int j = 1; j<5; j++){
-                String id = i+"."+j;
-                Button tap = new Button(String.valueOf(j));
-                buttonManager.put(id, tap);
+            for (int i = 0; i < 10; i++) {
+                for (int j = 1; j < 5; j++) {
+                    String id = i + "." + j;
+                    Button tap = new Button(String.valueOf(j));
+                    buttonManager.put(id, tap);
 
-                /*
-                int x = (int)(Math.random() * view.getWidth());
-                int y = (int)(Math.random() * view.getHeight());
-                tap.setLayoutX(x);
-                tap.setLayoutX(y);*/
+                    /*
+                    int x = (int)(Math.random() * view.getWidth());
+                    int y = (int)(Math.random() * view.getHeight());
+                    tap.setLayoutX(x);
+                    tap.setLayoutX(y);*/
 
-                  int x = (int)(Math.random() * view.getWidth());
-                  int y = (int)(Math.random() * view.getHeight());
-                  tap.setLayoutX(x);
-                  tap.setLayoutY(y);
-                  Circle circle  = new Circle(x, y, 50);
-                  circle.setLayoutX(x);
-                  circle.setLayoutY(y);
-                  Platform.runLater(() -> {
-                      view.getChildren().add(tap);
-                      view.getChildren().add(circle);
-                  });
+                    int x = (int) (Math.random() * view.getWidth() - view.getWidth()/2);
+                    int y = (int) (Math.random() * view.getHeight() - view.getHeight()/2);
+                    double z = view.getWidth()/2 - view.boss_view.getFitWidth();
 
-                  try {
-                      Thread.sleep(1000);
-                  } catch (InterruptedException e) {
-                      throw new RuntimeException(e);
-                  }
-                  Timer buttonDisable = new Timer();
-                buttonDisable.schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        Platform.runLater(() -> {
-                            if(view.getChildren().contains(buttonManager.get(id))) {
-                                view.getChildren().remove(buttonManager.get(id));
-                            }
+                    tap.setTranslateX(x);
+                    tap.setTranslateY(y);
+                    view.boss_view.setTranslateX(z);
 
+                    Platform.runLater(() -> {
+                        tap.getStyleClass().add("button-click");
+                        view.getChildren().add(tap);
+                    });
 
-                            tap.getStyleClass().add("button-click");
-                        });
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
                     }
-                }, 1000);
 
-                tap.setId(i+ "."+ j);
-                tap.setOnAction(e -> {
-                    move(hero_view);
-                    view.getChildren().remove(tap);
-                    buttonManager.remove(id);
+                    Timer buttonDisable = new Timer();
+                    buttonDisable.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            Platform.runLater(() -> {
+                                if (view.getChildren().contains(buttonManager.get(id))) {
+                                    view.getChildren().remove(buttonManager.get(id));
+                                }
+                            });
+                        }
+                    }, 3000);
 
-                });
-              }
+                    tap.setId(i + "." + j);
+                    tap.setOnAction(e -> {
+                        move(hero_view);
+                        view.getChildren().remove(tap);
+                        buttonManager.remove(id);
 
-          }
-      });
+                    });
+
+                }
+
+            }
+        });
     }
 
-    public GameScreenController () throws FileNotFoundException {
+    public GameScreenController() throws FileNotFoundException {
         this.view = new GameScreenView();
         this.hero_view = view.hero_view;
+        this.gameCanvas = view.gameCanvas;
+
         buttonSpawn().start();
         intialize();
     }
--- src/main/java/presentation/scenes/gameScreen/GameScreenView.java
+++ src/main/java/presentation/scenes/gameScreen/GameScreenView.java
@@ -1,5 +1,6 @@
 package presentation.scenes.gameScreen;
 
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -15,14 +16,17 @@ public class GameScreenView extends StackPane {
 
     public ImageView hero_view;
     public  ImageView boss_view;
+    public Canvas gameCanvas;
     public GameScreenView() throws FileNotFoundException {
         hero_view = new ImageView();
-        hero_view.setImage(new Image(new FileInputStream("src/main/resources/application/images/character1.png")));
+        hero_view.setImage(new Image(new FileInputStream("src/main/resources/application/images/hero_male.png")));
 
         boss_view = new ImageView();
         boss_view.setImage(new Image(new FileInputStream("src/main/resources/application/images/boss1.png")));
 
+        gameCanvas = new Canvas();
+
         this.getStyleClass().add("gamescreen");
-        this.getChildren().add(hero_view);
+        this.getChildren().addAll(hero_view, boss_view, gameCanvas);
     }
 }
