63063e18a9106b78e42841d7a112399da8b76c41
timreinicke <45878428+timreinicke@users.noreply.github.com>
2023-01-25 19:13:55 +0100

    Changed Button Spawn and added BeatDetection

--- src/main/java/presentation/scenes/gameScreen/GameScreenController.java
+++ src/main/java/presentation/scenes/gameScreen/GameScreenController.java
@@ -1,6 +1,10 @@
 package presentation.scenes.gameScreen;
 
+import com.mpatric.mp3agic.InvalidDataException;
+import com.mpatric.mp3agic.Mp3File;
+import com.mpatric.mp3agic.UnsupportedTagException;
 import ddf.minim.AudioInput;
+import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
 import ddf.minim.analysis.BeatDetect;
 import javafx.animation.TranslateTransition;
@@ -10,6 +14,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.util.Duration;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,7 +27,8 @@ public class GameScreenController extends Thread {
 
     private final GameScreenView view;
     private final ImageView hero_view;
-    StackPane buttonView;
+    private final StackPane buttonView;
+    Mp3File mp3File;
 
     public GameScreenController() throws FileNotFoundException {
         this.view = new GameScreenView();
@@ -34,40 +40,75 @@ public class GameScreenController extends Thread {
         intialize();
     }
 
-    public InputStream createInput(String filename) {
-        return new InputStream() {
-            @Override
-            public int read() throws IOException {
-                return 0;
+    public static class MinimInput {
+        public String sketchPath(String fileName) {
+            return "src/main/resources/application/music/tracks/track1.mp3";
+        }
+
+        public InputStream createInput(String fileName) {
+            InputStream is = null;
+            try {
+                is = new FileInputStream(sketchPath(fileName));
+            } catch (Exception e) {
+                System.out.println(e.toString());
             }
-        };
+            return is;
+        }
+
+        ;
     }
 
     public Thread buttonSpawn() {
         return new Thread(() -> {
+            try {
+                mp3File = new Mp3File("src/main/resources/application/music/tracks/track1.mp3");
+            } catch (IOException | UnsupportedTagException | InvalidDataException e) {
+                throw new RuntimeException(e);
+            }
 
             Map<String, Button> buttonManager = new HashMap<>();
-            Minim minim = new Minim(createInput("src/main/resources/application/music/tracks/blook.mp3"));
+            Minim minim = new Minim(new MinimInput());
             AudioInput input = minim.getLineIn(Minim.STEREO, 1024);
-
-            BeatDetect beat = new BeatDetect(1024, 44100.0f);
-            beat.setSensitivity(1000);
-            beat.detect(input.mix);
-            if (beat.isHat()) {
-                System.out.println("Hat");
-            }
-            for (int i = 0; i < 1; i++) {
+            AudioPlayer player;
+            int i = 0, j = 0, x = 0, y = 0;
 
 
-                for (int j = 1; j < 5; j++) {
+            player = minim.loadFile("src/main/resources/application/music/tracks/track1.mp3", 2048);
+            player.play();
+            player.setGain(-40);
+
+            BeatDetect beat = new BeatDetect(2048, 44100.0f);
+            beat.detectMode(BeatDetect.FREQ_ENERGY);
+            beat.setSensitivity(100);
+
+            Timer removeButtons = new Timer();
+            removeButtons.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    Platform.runLater(() -> {
+                        buttonView.getChildren().clear();
+                    });
+                }
+            }, mp3File.getLengthInMilliseconds());
+
+
+            while (true) {
+                beat.detect(player.mix);
+
+                if (beat.isRange(15, 20, 4)) {
+                    if (i == 0) {
+                        x = (int) (Math.random() * view.getWidth() - view.getWidth() / 2 - 250);
+                        y = (int) (Math.random() * view.getHeight() - view.getHeight() / 2 - 250);
+                    }
+
+                    i++;
+
+                    x = x + 90;
+                    y = y + 5;
 
                     String id = i + "." + j;
-                    Button tap = new Button(String.valueOf(j));
+                    Button tap = new Button(String.valueOf(i));
                     buttonManager.put(id, tap);
-
-                    int x = (int) (Math.random() * view.getWidth() - view.getWidth() / 2);
-                    int y = (int) (Math.random() * view.getHeight() - view.getHeight() / 2);
-
 
                     double z = view.getWidth() / 2 - view.boss_view.getFitWidth();
 
@@ -93,41 +134,28 @@ public class GameScreenController extends Thread {
                         public void run() {
                             Platform.runLater(() -> {
                                 if (buttonView.getChildren().contains(buttonManager.get(id))) {
-                                   // buttonView.getChildren().remove(buttonManager.get(id));
+                                    // buttonView.getChildren().remove(buttonManager.get(id));
                                     buttonManager.remove(id);
-
                                 }
                                 tap.setDisable(true);
                             });
-
                         }
                     }, 3000);
 
                     tap.setId(i + "." + j);
                     tap.setOnAction(e -> {
-                        Platform.runLater(() -> {
-                            move(hero_view);
-                           /* view.getChildren().remove(tap);
-                            buttonManager.remove(id);*/
-                            tap.setDisable(true);
-                        });
+                        move(hero_view);
+                        tap.setDisable(true);
                     });
 
+                    if (i == 4) {
+                        i = 0;
+                        j++;
+                    }
                 }
             }
-
-            Timer removeButtons = new Timer();
-            removeButtons.schedule(new TimerTask() {
-                @Override
-                public void run() {
-                    Platform.runLater(() -> {
-                        buttonView.getChildren().clear();
-                    });
-                }
-            }, 12000);
         });
     }
-
 
     public void move(ImageView image) {
         TranslateTransition translate = new TranslateTransition();
@@ -148,6 +176,4 @@ public class GameScreenController extends Thread {
     public GameScreenView getView() {
         return view;
     }
-
-
 }
